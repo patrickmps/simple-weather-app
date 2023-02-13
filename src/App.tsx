a61c180e3bc5c43f885623f6858b6b796e8a58d3
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-02-12 23:25:34 -0300

    feat: fetch api, styles and structure
    Exibição informações da api.
    Tipagem dos dados.
    Conexão com api das bandeiras.
    Estrutura HTML e estilização.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import './global.scss'
 import { SimpleWeather } from "./components/SimpleWeather";
-
 
 function App() {
 
--- src/components/SimpleWeather/index.tsx
+++ src/components/SimpleWeather/index.tsx
@@ -1,17 +1,39 @@
 import { useApi } from "../../hooks/useApi";
 import "./style.scss";
 
-
-
 export const SimpleWeather = ({}) => {
   const { data } = useApi<OpenWeatherProps>(
     "https://api.openweathermap.org/data/2.5/weather?q=jequie&appid=02a090af1201f342e9b2df360e1d954c&units=metric&lang=pt_br"
   );
-  console.log(data);
 
   return (
-    <div>
-
+    <div className="main-container">
+      <span className="line-city">
+        <h1>{data?.name}</h1>
+        {data && <img crossOrigin="anonymous" src={`https://countryflagsapi.com/png/${data?.sys.country.toLowerCase()}`} alt="" />}
+      </span>
+      <div className="weather">
+        <div id="wheater-desc">
+          <img src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`} alt="" />
+          <strong>{data?.weather[0].description}</strong>
+        </div>
+        <div id="temp">
+          <p>{`Sensação de ${data?.main.feels_like}° C`}</p>
+          <strong>{data && Math.round(data?.main.temp)}° C</strong>
+        </div>
+      </div>
+      <span className="line-info">
+        <h2>Humidade</h2>
+        <strong>{data?.main.humidity}</strong>
+      </span>
+      <span className="line-info">
+        <h2>Visibilidade</h2>
+        <strong>{data && `${(data?.visibility)/1000}km`}</strong>
+      </span>
+      <span className="line-info">
+        <h2>Vento</h2>
+        <strong>{`${data?.wind.speed}m/s`}</strong>
+      </span>
     </div>
-  )
-}
+  );
+};
